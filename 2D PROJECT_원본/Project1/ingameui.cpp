9212776da6054dfa91f5564a641a090a47947629
yj4029 <yj4029@gmail.com>
2018-10-17 20:32:19 +0900

    ㄱ

--- 2D PROJECT_원본/Project1/ingameui.cpp
+++ 2D PROJECT_원본/Project1/ingameui.cpp
@@ -8,16 +8,16 @@ HRESULT ingameui::init()
 	m_pImgInGameUi2 = new image;
 
 	m_pImgInGameUi = IMAGEMANAGER->addImage("inGameUi", "image/ui/ingameui.bmp",
-		164, 75, true, RGB(255, 0, 255));
+		162, 60, true, RGB(255, 0, 255));
 	m_pImgInGameUi2 = IMAGEMANAGER->addImage("inGameUI2", "image/ui/ui.bmp",
 		500, 251, true, RGB(255, 0, 255));
 
 	m_UifX1 = 50;
 	m_UifY1 = 20;
 
-	m_UifX2 = 0;
-	m_UifY2 = 0; 
-
+	m_UifX2 = 100;
+	m_UifY2 = 50; 
+	
 	return S_OK;
 }
 
@@ -35,7 +35,8 @@ void ingameui::update()
 void ingameui::render(HDC hdc)
 {
 	m_pImgInGameUi->render(hdc, m_UifX1, m_UifY1, 0, 0, 141, 29, 2);
-	m_pImgInGameUi->render(hdc, m_UifX1+450, m_UifY1, 0, 29, 164, 46, 2);
+	m_pImgInGameUi->render(hdc, m_UifX1+450, m_UifY1, 0, 29, 162, 31, 2);
+	m_pImgInGameUi2->render(hdc, m_UifX2, m_UifY2, 0, 240, 8, 8, 2);
 }
 
 ingameui::ingameui()
